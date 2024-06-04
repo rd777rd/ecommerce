be5a18be133ecc32d0ddc9f516ed5270eb9ef426
Romsites LLC <77874924+rd777rd@users.noreply.github.com>
2024-06-04 10:57:40 -0400

    Update shop.jsx

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -3,13 +3,11 @@ import {PRODUCTS} from "../../products";
 import {Product} from "./product";
 import "./shop.css";
  import axios from 'axios';
- import {Link, useNavigate} from 'react-router-dom';
 
 export const Shop = () => {
    const [auth, setAuth] = useState("");
   const [message, setMessage] = useState('')
   const[name, setName] = useState('')
-  const navigate = useNavigate();
   axios.defaults.withCredentials = true;
   useEffect(()=>{
     axios.get('http://localhost:8081')
@@ -33,12 +31,26 @@ export const Shop = () => {
         <div className="shopTitleContainer">
             <h1 className="shopTitle">Ecommerce</h1>
         </div>
+        {
+          auth ?
+              <div className="products">
+               <h2>Welcome Back {name}</h2>
+                {PRODUCTS.map((product) =>(
+                    <Product data={product} />
+                ))}
+            </div>
+
+        
+        :
+  
+ 
         <div className="products">
-          
+          <h2>{message}</h2>
             {PRODUCTS.map((product) =>(
                 <Product data={product} />
             ))}
         </div>
+}
     </div>
             
   )
