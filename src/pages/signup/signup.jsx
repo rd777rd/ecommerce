a48413ef1966f0ef8b7695c16839649c91c038e3
Romsites LLC <77874924+rd777rd@users.noreply.github.com>
2024-06-26 18:57:08 -0400

    Update signup.jsx

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -25,7 +25,7 @@ const [errors, setErrors] = useState({})
     const err= Validation(values);
     setErrors(err);
     if(err.firstName === "" && err.lastName ==="" && err.birthDate === "" && err.address ==="" && err.email === "" && err.password ==="") {
-      axios.post('http://localhost:3000/signup', values)
+      axios.post('https://ecommercedb-q0qz.onrender.com/login', values)
       .then(res => {
         navigate('/login');
       })
